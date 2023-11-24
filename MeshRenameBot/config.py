7ad2d590a0352902da0241101d61683a415deeec
Ashutosh15rai <115929606+Ashutosh15rai@users.noreply.github.com>
2023-11-24 13:11:06 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -4,10 +4,10 @@ try:
     from .tconfig import Config
 except ImportError:
     class Config:
-        DATABASE_URL = [str, "postgres://iubxmjfs:XtftA36D6kajzosJinXlepPr40xId6Df@trumpet.db.elephantsql.com/iubxmjfs"]
-        API_HASH = [str, "2b445de78e5baf012a0793e60bd4fbf5"]
-        API_ID = [int, 19099900]
-        BOT_TOKEN = [str, "6295293651:AAEMIVNFEgMwiuzPEoWLRU9arF9kbPzOrA0"]
+        DATABASE_URL = [str, "postgres://mbriubpn:m2tWxb_RrtWOhqlFCCm000JEpLpAtZZo@tai.db.elephantsql.com/mbriubpn"]
+        API_HASH = [str, "90da70b17365b3709b8a0346a7749ce7"]
+        API_ID = [int, 20421248]
+        BOT_TOKEN = [str, "6633234800:AAGOiWkNZg_E31Q9FZatrW979esrWP5GJ1A"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
         MAX_QUEUE_SIZE = [int, 5]
@@ -16,14 +16,14 @@ except ImportError:
 
         # Access Restriction
         IS_PRIVATE = [bool, False]
-        AUTH_USERS = [list,[6295293651]]
+        AUTH_USERS = [list,[5009250822]]
         OWNER_ID = [int, 0]
 
         # Public username url or invite link of private chat
-        FORCEJOIN = [str,"https://t.me/illuminatiXNetwork"]
-        FORCEJOIN_ID = [int,-1002058050288]
+        FORCEJOIN = [str,"https://t.me/+VaTnv2H60IExZDY1"]
+        FORCEJOIN_ID = [int,-1001863995845]
 
-        TRACE_CHANNEL = [int, -1002058050288]
+        TRACE_CHANNEL = [int, -1002001018139]
 
 try:
     from .tconfig import Commands
